b0982ee9419fbd0aa80029afa29c4bfb0d3c1cb1
ivan232355 <91849543+ivan232355@users.noreply.github.com>
2023-10-28 21:01:57 +0300

    Add files via upload

---
+++ Embedded/Lesson_1/Honey.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf (" __    __\n");
+    printf ("/  \\__/  \\ \n");
+    printf ("\\__/  \\__/\n");
+    printf ("/  \\__/  \\ \n");
+    printf ("\\__/  \\__/\n");
+	return 0;
+}
+
