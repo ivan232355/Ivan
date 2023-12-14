a2c1ab2d9d4773ae5f1296d6c41874b59e0b9a18
ivan232355 <91849543+ivan232355@users.noreply.github.com>
2023-12-14 14:47:33 +0300

    Add files via upload

--- Embedded/Lesson_9/F8_2.c
+++ Embedded/Lesson_9/F8_2.c
@@ -2,32 +2,38 @@
 F8 Практ 2
 Пропущенное число
 Написать функцию и программу, демонстрирующую работу данной функции.
-В последовательности записаны целые числа от M до N ( M меньше N, M больше или равно 1) в произвольном порядке, но одно из чисел пропущено (остальные встречаются ровно по одному разу). N не превосходит 1000. Последовательность заканчивается числом 0. Определить пропущенное число. 
+В последовательности записаны целые числа от M до N ( M меньше N, M больше или равно 1) в произвольном порядке,
+но одно из чисел пропущено (остальные встречаются ровно по одному разу).
+N не превосходит 1000. Последовательность заканчивается числом 0. Определить пропущенное число.
 
-Данные на входе: 		Последовательность целых чисел от M до N. M, N не превосходит 1000. 
-Данные на выходе: 	Одно целое число 
+Данные на входе: 		Последовательность целых чисел от M до N. M, N не превосходит 1000.
+Данные на выходе: 	Одно целое число
 
 Пример №1
-Данные на входе: 		2 3 1 4 7 6 9 8 10 0 
-Данные на выходе: 	5 
+Данные на входе: 		2 3 1 4 7 6 9 8 10 0
+Данные на выходе: 	5
 
 Пример №2
-Данные на входе: 		41 40 39 38 37 36 34 33 32 31 50 49 48 47 46 45 44 43 42 30 0 
-Данные на выходе: 	35 
+Данные на входе: 		41 40 39 38 37 36 34 33 32 31 50 49 48 47 46 45 44 43 42 30 0
+Данные на выходе: 	35
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <inttypes.h>
 
-int s[100];
+int s[10] = {2,3,1,4,7,6,9,8,10};
+//int s[20] = {41,40,39,38,37,36,34,33,32,31,50,49,48,47,46,45,44,43,42,30};
+//int s[10];
 int size;
 int number = 0;
 
-void find_sum_max_min(int arr[], int size, int *max, int *min, int* sum)
+//Функция нахождения максимума и минимума и вычисления суммы чисел найденного интервала
+int find_sum_max_min(int arr[], int size, int *max, int *min)
 {
     *max = arr[0];
     *min = arr[0];
+    int sum;
 
     for (int i = 1; i < size; i++)
     {
@@ -40,15 +46,14 @@ void find_sum_max_min(int arr[], int size, int *max, int *min, int* sum)
             *min = arr[i];
         }
     }
-    printf("min = %d\n",*min);
-    printf("max = %d\n",*max);
-	for (int i = *min; i < *max; i++)
+    sum = *min;
+	for (int i = *min + 1; i <= *max; i++)
     {
-        sum += 1;
+        sum += i;
     }
-	printf("sum = %d\n",*sum);
+	return sum;
 }
-
+// Функция нахождения недостающего числа
 int find_number(int arr[], int size)
 {
     int sum = 0;
@@ -60,12 +65,11 @@ int find_number(int arr[], int size)
         sum += arr[i];
     }
 
-	find_sum_max_min(arr, size,&end_arr, &start_arr,&sum_max_arr);
-	printf("sum_max_arr = %d\n",sum_max_arr);
+    sum_max_arr = find_sum_max_min(arr, size,&end_arr, &start_arr);
     return sum_max_arr - sum;
 }
 
-
+// Функция нахождения одинаковых чисел
 int is_two_same(int size, int a[])
 {
     for (int i = 0; i < size - 1; i++)
@@ -80,14 +84,15 @@ int is_two_same(int size, int a[])
     }
     return 0;
 }
+// Проверка чисел больше 1000
 int checking_number(int size, int a[])
 {
     for (int i = 0; i < size - 1; i++)
     {
 		if (a[i] > 1000)
 		{
-          printf(">1000!!!\n");
-		return 0;
+          printf("%d>1000!!!\n",a[i]);
+          return 0;
 		}
     }
     return 1;
@@ -125,7 +130,8 @@ char c;
 
 int main()
 {
-    size = InputGet(s, 100);
+    //size = InputGet(s, 100);
+	size = sizeof(s)/sizeof(s[0]);
 
     if (!checking_number(size, s)) 		// проверяем, что все числа в массиве меньше или равны 1000
     {
@@ -139,11 +145,10 @@ int main()
     else
     {
         number = find_number(s,size); 	// находим недостающее число
-        printf("%d",number); 			
+        printf("%d",number);
     }
     return 0;
 }
-
 
 
 
